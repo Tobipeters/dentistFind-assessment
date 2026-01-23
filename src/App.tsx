cbb9d3615f1fed81bdf266987dcd94d711a6d209
Tobi Peter <oluwatobilobapeterschem@gmail.com>
2026-01-23 12:12:36 +0100

    Corrected a typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <main className="container flex flex-col w-full gap-6 mx-auto p-5 lg:px-6 lg:py-8">
       <h2 className="text-2xl lg:text-3xl font-semibold text-gray-900">
-        Practive Overview
+        Practice Overview
       </h2>
       <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {practiceSummarys.map((practice, id) => (
